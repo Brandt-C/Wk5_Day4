f1261bdea3e73772089b11243a250afff3724184
Brandt-C <brandt.carlson11@gmail.com>
2024-04-02 15:02:56 -0500

    folder structure changed

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,10 @@ from flask import Flask
 from flask_smorest import Api
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from app.config import Config
-from app.models.car_model import CarModel
-from app.resources.sale_receipt import bp as sale_receipt_bp
-from app.resources.car import bp as car_bp
+from Config import Config
+from models.car_models import CarModel
+from resources.sale_receipt import bp as sale_receipt_bp
+from resources.car import bp as car_bp
 
 app = Flask(__name__)
 app.config.from_object(Config)
